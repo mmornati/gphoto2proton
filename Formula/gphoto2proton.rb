00bb843479a13744a7fac948ed8336c48a48db37
mmornati <139323+mmornati@users.noreply.github.com>
2026-07-31 06:17:18 +0000

    chore(homebrew): update formula to v0.1.1

--- Formula/gphoto2proton.rb
+++ Formula/gphoto2proton.rb
@@ -3,27 +3,27 @@ class Gphoto2proton < Formula
   homepage "https://github.com/mmornati/gphoto2proton"
   license "MIT"
 
-  version "0.1.0"
+  version "0.1.1"
 
   on_macos do
     on_arm do
-      url "https://github.com/mmornati/gphoto2proton/releases/download/v0.1.0/gphoto2proton_0.1.0_darwin_arm64.tar.gz"
-      sha256 "c3fb9381935a8e3e1bb23d11a115d62626ce6d746279a6dd596396ba0ddaf7c5"
+      url "https://github.com/mmornati/gphoto2proton/releases/download/v0.1.1/gphoto2proton_0.1.1_darwin_arm64.tar.gz"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
     on_intel do
-      url "https://github.com/mmornati/gphoto2proton/releases/download/v0.1.0/gphoto2proton_0.1.0_darwin_amd64.tar.gz"
-      sha256 "ebbf112dece24d254602b0f7da4468cded885cbc8f4aff013986a0e542a39cf7"
+      url "https://github.com/mmornati/gphoto2proton/releases/download/v0.1.1/gphoto2proton_0.1.1_darwin_amd64.tar.gz"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/mmornati/gphoto2proton/releases/download/v0.1.0/gphoto2proton_0.1.0_linux_arm64.tar.gz"
-      sha256 "85eea1c636b3263daa2eb117f078a1161f1d2e503442e2d24ac563226e40fe39"
+      url "https://github.com/mmornati/gphoto2proton/releases/download/v0.1.1/gphoto2proton_0.1.1_linux_arm64.tar.gz"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
     on_intel do
-      url "https://github.com/mmornati/gphoto2proton/releases/download/v0.1.0/gphoto2proton_0.1.0_linux_amd64.tar.gz"
-      sha256 "ba724f08ff00c38a37d9dfcde8201b48aa38c82cb40591d2202bb395cbffa509"
+      url "https://github.com/mmornati/gphoto2proton/releases/download/v0.1.1/gphoto2proton_0.1.1_linux_amd64.tar.gz"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
