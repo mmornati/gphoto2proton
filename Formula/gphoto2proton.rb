ae943097ca612c9ef8f5049098771a6bd5ac2fc7
mmornati <139323+mmornati@users.noreply.github.com>
2026-08-02 09:54:12 +0000

    chore(homebrew): update formula to v0.3.0

--- Formula/gphoto2proton.rb
+++ Formula/gphoto2proton.rb
@@ -3,26 +3,26 @@ class Gphoto2proton < Formula
   homepage "https://github.com/mmornati/gphoto2proton"
   license "MIT"
 
-  version "0.2.1"
+  version "0.3.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/mmornati/gphoto2proton/releases/download/v0.2.1/gphoto2proton_0.2.1_darwin_arm64.tar.gz"
+      url "https://github.com/mmornati/gphoto2proton/releases/download/v0.3.0/gphoto2proton_0.3.0_darwin_arm64.tar.gz"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
     on_intel do
-      url "https://github.com/mmornati/gphoto2proton/releases/download/v0.2.1/gphoto2proton_0.2.1_darwin_amd64.tar.gz"
+      url "https://github.com/mmornati/gphoto2proton/releases/download/v0.3.0/gphoto2proton_0.3.0_darwin_amd64.tar.gz"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/mmornati/gphoto2proton/releases/download/v0.2.1/gphoto2proton_0.2.1_linux_arm64.tar.gz"
+      url "https://github.com/mmornati/gphoto2proton/releases/download/v0.3.0/gphoto2proton_0.3.0_linux_arm64.tar.gz"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
     on_intel do
-      url "https://github.com/mmornati/gphoto2proton/releases/download/v0.2.1/gphoto2proton_0.2.1_linux_amd64.tar.gz"
+      url "https://github.com/mmornati/gphoto2proton/releases/download/v0.3.0/gphoto2proton_0.3.0_linux_amd64.tar.gz"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
